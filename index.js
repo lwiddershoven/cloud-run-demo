496f867a84c6f44bcda2bcb86bb66e8ac235c4c2
Leon <qaz@dds.nl>
2020-11-18 14:54:25 +0100

    update

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const app = express();
 app.get('/', (req, res) => {
   console.log('Hello world received a request.');
 
-  res.send(`Hello Someone!`);
+  res.send(`Hello Leon!`);
 });
 
 const port = process.env.PORT || 8080;
